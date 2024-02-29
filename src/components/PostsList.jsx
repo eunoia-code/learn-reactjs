6cfa0903d28f95a484b05fac93420c4942d17d16
Rizaldy Setiawan Hasanuddin <rsh.newarray@gmail.com>
2024-02-29 15:06:47 +0800

    36. Sending a POST HTTP Request

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -8,7 +8,14 @@ export default function PostsList({isPosting, onStopPosting}) {
   const [posts, setPosts] = useState([])
   
   function addPostHandler(postData) {
-    setPosts((existingPosts) => [postData, ...existingPosts])
+    fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    // setPosts((existingPosts) => [postData, ...existingPosts])
   }
 
   return (
