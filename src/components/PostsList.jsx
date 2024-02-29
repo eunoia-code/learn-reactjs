3aa09279d6447133bdf85a82a7c3f707b9f28775
Rizaldy Setiawan Hasanuddin <rsh.newarray@gmail.com>
2024-02-29 11:48:54 +0800

    34. Outputting List Data

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -21,10 +21,19 @@ export default function PostsList({isPosting, onStopPosting}) {
           />
         </Modal>
       )}
-      <ul className={styles.posts}>
-        <Post name="Arteezy" content="Man Upon A Cliffs" />
-        <Post name="Miracle-" content="Backpain Coz Kuroky Wont Retired" />
-      </ul>
+      { posts.length > 0 && (
+          <ul className={styles.posts}>
+            {posts.map((post, index) => (
+              <Post key={index} name={post.name} content={post.content} />
+            ))}
+          </ul>
+      )}
+      { posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white'}}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )} 
     </>
   )
 }
