ea52d0956ca4a44ecc2e08b23a7b2dc0688cc83e
Rizaldy Setiawan Hasanuddin <rsh.newarray@gmail.com>
2024-02-29 09:43:28 +0800

    31. Handling Form Submission

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,37 +6,16 @@ import Modal from './Modal';
 
 export default function PostsList({isPosting, onStopPosting}) {
 
-  const [contentValue, setContentValue] = useState('')
-  const [nameValue, setNameValue] = useState('')
-
-  function contentChangeHandler(event) {
-    setContentValue(event.target.value)
-  }
-
-  function nameChangeHandler(event) {
-    setNameValue(event.target.value)
-  }
-
-  // let modalContent;
-  // if(modalIsVisible){
-  //   modalContent = <Modal onClose={modalHandler}>
-  //                   <NewPost onContentChange={contentChangeHandler} onNameChange={nameChangeHandler} />
-  //                 </Modal>
-  // }
-
   return (
     <>
       { isPosting && (
         <Modal onClose={onStopPosting}>
           <NewPost 
-            onContentChange={contentChangeHandler} 
-            onNameChange={nameChangeHandler} 
             onCancel={onStopPosting}
           />
         </Modal>
       )}
       <ul className={styles.posts}>
-        <Post name={nameValue} content={contentValue} />
         <Post name="Arteezy" content="Man Upon A Cliffs" />
         <Post name="Miracle-" content="Backpain Coz Kuroky Wont Retired" />
       </ul>
